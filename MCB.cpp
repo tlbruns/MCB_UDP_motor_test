e14fba6accf7efa7192937db7f6eb0954f05238c
tlbruns <t.bruns@vanderbilt.edu>
2017-03-31 16:17:29 -0500

    Add project files.

---
+++ MCB.cpp
@@ -0,0 +1,197 @@
+/*=========================================================================//
+
+	MCB Class
+	
+	This class handles the modules plugged into the motor controller board
+	
+	Designed for use with Teensy 3.1 and Motor Control Board (Rev 1.1)
+	
+	
+	Trevor Bruns
+	
+	Changelog-
+		2/20/2016: Initial Creation
+		3/13/2017: Compiles and appears to run successfully
+		3/30/2017: Corrected byte order for setting DACs
+		
+//========================================================================*/
+
+#include "MCB.h"
+#include "MCBmodule.h"
+#include <ArduinoSTL.h> 
+#include "si5351.h"
+#include <SPI.h>
+
+MCB::MCB(uint8_t numModules)
+// will initialize modules using default pins (still need to call init after)
+	: DAC_(pins.csDAC)
+	, numModules_(numModules)	
+{	
+	// reserve memory for module components
+	modules.reserve(numModules_);
+	DACval_.reserve(numModules_);
+}
+
+void MCB::init(void)
+// initializes all modules (to be called only after all addModule commands)
+{
+
+	// initialize MCB pins
+	pins.init();
+
+	// initialize encoder clock for LS7366R
+	si5351_.init(SI5351_CRYSTAL_LOAD_8PF, 0);
+	si5351_.set_freq(3000000000ULL, 0ULL, SI5351_CLK0); // Set CLK1 to output 30 MHz
+	si5351_.output_enable(SI5351_CLK1, 0); // Disable other clocks
+	si5351_.output_enable(SI5351_CLK2, 0);
+	
+	// initialize SPI
+	SPI.begin();
+	
+	// create and initialize each module (along with each encoder IC)
+	for (uint8_t aa = 0; aa < numModules_; aa++)
+	{
+		addModule(aa);
+	}
+	
+	// initialize DACs
+	DAC_.beginTransfer();
+	for (uint8_t bb = 0; bb < numModules_; bb++)
+	{
+		DAC_.reset(); // software reset
+	}
+	DAC_.endTransfer();
+	
+	DAC_.beginTransfer();
+	for (uint8_t bb = 0; bb < numModules_; bb++)
+	{
+		DAC_.init(); // setup ctrl register
+	}
+	DAC_.endTransfer();
+
+	DAC_.beginTransfer();
+	for (uint8_t bb = 0; bb < numModules_; bb++)
+	{
+		DAC_.set(0); // Set output to 0 volts
+	}
+	DAC_.endTransfer();
+}
+
+void MCB::addModule(uint8_t position)
+{
+	modules.push_back(MCBmodule(position)); // create new MCBmodule and add to storage vector
+	modules.at(position).init(); // initialize module
+
+	DACval_.push_back(0);   // initialize DAC output values to 0
+}
+
+void MCB::disableAllAmps(void)
+{
+	for (uint8_t aa = 0; aa < pins.maxNumBoards; aa++)
+	{
+		// software brakes (LOW = amps disabled)
+		digitalWriteFast(pins.ampEnable[aa], LOW);
+	}
+}
+
+void MCB::enableAllAmps(void)
+{
+	// enable motor amp outputs and turn on green LEDs
+	for (uint8_t aa = 0; aa < numModules_; aa++)
+	{
+		// software brakes (HIGH = amps enabled)
+		digitalWriteFast(pins.ampEnable[aa], HIGH);
+		setLEDG(aa, HIGH);
+	}
+}
+
+void MCB::stepPID(void)
+{
+	
+	// step PID controllers
+	for (uint8_t aa = 0; aa < modules.size(); aa++)
+	{
+		DACval_.at(aa) = modules.at(aa).step();
+	}
+	
+	// update DACs
+	setDACs(DACval_);
+}
+
+void MCB::setDACs(Int16Vec const &val)
+{
+
+	DAC_.beginTransfer();
+
+	// send out in reverse order (data is pushed through the daisy chain)
+	for (uint8_t bb = val.size(); bb > 0; bb--)
+	{
+		DAC_.set(val.at(bb - 1));
+	}
+
+	DAC_.endTransfer();
+}
+
+void MCB::setLEDG(uint8_t position, bool state)
+{
+	LEDG_.at(position) = state;
+	digitalWriteFast(pins.LEDG[position], LEDG_.at(position));
+}
+
+void MCB::setCount(uint8_t moduleNum, int32_t countDesired)
+{
+	
+	modules.at(moduleNum -1).setCountDesired(countDesired);
+}
+
+Int32Vec MCB::getCounts(void)
+{
+	Int32Vec temp;
+	for (uint8_t aa = 0; aa < modules.size(); aa++)
+	{
+		temp.at(aa) = modules.at(aa).getCountLast();
+	}
+	
+	return temp;
+}
+
+int32_t MCB::getCount(uint8_t moduleNum)
+{	
+	return modules.at(moduleNum).getCountLast();
+}
+
+Uint32Vec MCB::readButtons(void)
+{	
+	uint8_t numButtons = sizeof(pins.buttons);
+	Uint32Vec buttonValues;
+	buttonValues.resize(numButtons);
+	
+	for (uint8_t aa = 0; aa < numButtons; aa++)
+	{
+		buttonValues.at(aa) = touchRead(pins.buttons[aa]);
+		pins.buttonStates[aa] = (buttonValues[aa] > pins.buttonThresh[aa]);
+	}
+	
+	return buttonValues;
+}
+
+bool MCB::isDownPressed(void)
+{
+	return pins.buttonStates[0];
+}
+
+bool MCB::isUpPressed(void)
+{
+	return pins.buttonStates[1];
+}
+
+bool MCB::isMenuPressed(void)
+{
+	return pins.buttonStates[2];
+}
+
+MCB::~MCB(void)
+{
+	// ensure all amps are disabled
+	disableAllAmps();
+}
---
+++ MCB_UDP_motor_test.ino
@@ -0,0 +1,167 @@
+#include <i2c_t3.h>
+#include <SPI.h>
+#include <ArduinoSTL.h>
+#include "LS7366R.h"
+#include "AD5761R.h"
+#include "si5351.h"
+#include "MCB.h"
+#include "MCBmodule.h"
+#include "MCBpins.h"
+#include "PID_f32.h"
+#include <arm_math.h>
+#include <IntervalTimer.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h> 
+
+// GLOBAL VARIABLES
+
+// Motor Control Board
+const uint8_t MCBmodules_num = 6; // number of modules (i.e. motors) plugged into this board
+MCB MotorBoard(MCBmodules_num);	// construct motor control board
+
+// toggle LEDs for timing w/oscilloscope
+bool pidLedState = false;
+bool udpLedState = false;
+bool buttonLedState = false;
+
+// PID timer interrupt
+IntervalTimer PIDTimer;
+void PIDTimerISR(void);
+uint32_t timeStepPID = 1000; // [us]
+float kp = 0.0004, ki = 0.000002, kd = 0.01; // work well for 1 kHz (1000 us)
+//uint32_t timeStepPID = 500; // [us]
+//float kp = 0.0002, ki = 0.000001, kd = 0.01; // work ok for 2 kHz (500 us)
+
+// UDP timer interrupt
+IntervalTimer UDPTimer;
+void UDPTimerISR(void);
+uint32_t timeStepUDP = 5000; // [us]
+
+// Button read timer interrupt
+IntervalTimer buttonTimer;
+void buttonTimerISR(void);
+uint32_t timeStepButton = 10000; // [us]
+uint32_t buttonCountChange = 500; // [counts]
+
+// sine wave trajectory
+float phase = 0.0;
+const int32_t sinAmplitude = 10000; // [counts]
+const float twopi = 2 * PI;
+int32_t countDesired = 0;
+
+// Ethernet
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+IPAddress ip(192, 168, 1, 177);
+unsigned int localPort = 8888;
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; // buffer to hold incoming UDP packet
+EthernetUDP Udp;
+
+//----------------------------------------------------------------//
+void setup()
+{
+	MotorBoard.init();
+	
+	for (int ii = 0; ii < MCBmodules_num; ii++)
+	{
+		MotorBoard.modules.at(ii).setGains(kp, ki, kd);
+		MotorBoard.modules.at(ii).setCountDesired(countDesired);
+	}
+
+	MotorBoard.enableAllAmps(); // enable amp
+
+	Ethernet.begin(mac, ip); // start Ethernet and UDP
+	Udp.begin(localPort);
+
+	PIDTimer.begin(PIDTimerISR, timeStepPID); // attach function and call every timeStepPID [us]
+	UDPTimer.begin(UDPTimerISR, timeStepUDP);
+	buttonTimer.begin(buttonTimerISR, timeStepButton);
+}
+
+//----------------------------------------------------------------//
+
+void loop()
+{
+	// everything is interrupt driven
+}
+
+//----------------------------------------------------------------//
+
+void PIDTimerISR(void)
+{
+	cli(); // disable all interrupts to ensure this process completes sequentially
+
+	pidLedState = !pidLedState; // blink LED for timing with oscilloscope
+	MotorBoard.setLEDG(0, pidLedState);
+
+	MotorBoard.stepPID();
+	
+	pidLedState = !pidLedState;
+	MotorBoard.setLEDG(0, pidLedState);
+
+	sei();
+}
+
+//----------------------------------------------------------------//
+
+void UDPTimerISR(void)
+{
+	cli(); // disable all interrupts to ensure this process completes sequentially
+
+	udpLedState = !udpLedState;
+	MotorBoard.setLEDG(1, udpLedState);
+
+	// if there's data available, read a packet
+	int packetSize = Udp.parsePacket();
+	if (packetSize) {
+		Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+		
+		for (int ii = 0; ii < MCBmodules_num; ii++)
+		{
+			MotorBoard.modules.at(ii).setCountDesired(packetBuffer[ii] * 50);
+
+		}
+	}
+
+	udpLedState = !udpLedState;
+	MotorBoard.setLEDG(1, udpLedState);
+
+	sei();
+}
+
+//----------------------------------------------------------------//
+
+void buttonTimerISR(void)
+{
+	buttonLedState = !buttonLedState; // blink LED for timing with oscilloscope
+	MotorBoard.setLEDG(2, buttonLedState);
+
+	// check Local <-> Remote switch on motherboard
+	if (!digitalReadFast(MotorBoard.pins.CTRL)) { // 'Remote' option
+		// generate sin wave trajectory
+		for (int ii = 0; ii < MCBmodules_num; ii++)
+		{
+			countDesired = (int32_t)(arm_sin_f32(phase + 0.25*ii) * sinAmplitude);
+			MotorBoard.modules.at(ii).setCountDesired(countDesired);
+		}
+		
+		phase = phase + 0.02;
+		if (phase >= twopi) phase = 0.0;
+	}
+	else { // 'Local' option
+		MotorBoard.readButtons();
+		if (MotorBoard.isUpPressed()) {
+			countDesired += buttonCountChange;
+			MotorBoard.modules.at(0).setCountDesired(countDesired);
+		}
+		else if (MotorBoard.isDownPressed()) {
+			countDesired -= buttonCountChange;
+			MotorBoard.modules.at(0).setCountDesired(countDesired);
+		}
+		else if (MotorBoard.isMenuPressed()) {
+			MotorBoard.disableAllAmps();
+		}
+	}
+
+	buttonLedState = !buttonLedState;
+	MotorBoard.setLEDG(2, buttonLedState);
+}
